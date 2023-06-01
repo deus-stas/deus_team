dc942ef0c07d0ee60d8021063a5abec0495991b0
SerdarHoja <serdarhoja@gmail.com>
2023-06-01 17:25:51 +0500

    login expire time changed to one day

--- routes/api/users.js
+++ routes/api/users.js
@@ -146,7 +146,7 @@ router.post("/login", (req, res) => {
               payload,
               keys.secretOrKey,
               {
-                expiresIn: 31556926 // 1 year in seconds
+                expiresIn: 86400 // 1 year in seconds
               },
               (err, token) => {
                 res.json({
@@ -176,7 +176,7 @@ router.post("/login", (req, res) => {
                 payload,
                 keys.secretOrKey,
                 {
-                  expiresIn: 31556926 // 1 year in seconds
+                  expiresIn: 86400 // 1 year in seconds
                 },
                 (err, token) => {
                   res.json({
