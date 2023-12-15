87f2457d85a2ef1ebbb85f5b28cf3cbea3c1adb2
Selezneva Anna <sovvysha@yandex.ru>
2023-12-15 14:24:54 +0300

    update

--- client/src/components/appHeader/AppHeader.js
+++ client/src/components/appHeader/AppHeader.js
@@ -150,19 +150,21 @@ const AppHeader = (props) => {
 
                                 {headerData && headerData.phone ?
                                     (
-                                        <div className="header__contacts hidden-mobile">
+                                        <div className="header__contacts hover-flip hidden-mobile">
                                             {/*<Link to={`mailto:${headerData.email}`}*/}
                                             {/*      className="header__contacts-link">{headerData.email}</Link>*/}
                                             <DelayedLink to={`tel:${headerData.phone}`}
-                                                  className="header__contacts-link">{headerData.phone}</DelayedLink>
+                                                  className="header__contacts-link">
+                                                <span data-hover={headerData.phone}> {headerData.phone}</span>
+                                               </DelayedLink>
                                         </div>
                                     ) :
                                     (
-                                        <div className="header__contacts hidden-mobile">
+                                        <div className="header__contacts hover-flip hidden-mobile">
                                             {/*<Link to="mailto:hello@de-us.ru"*/}
                                             {/*      className="header__contacts-link">hello@de-us.ru</Link>*/}
-                                            <DelayedLink to="tel:+74951034351" className="header__contacts-link">+7 (495)
-                                                103—4351</DelayedLink>
+                                            <DelayedLink to="tel:+74951034351" className="header__contacts-link">
+                                                <span data-hover="+7 (495) 103—4351 ">+7 (495) 103—4351 </span></DelayedLink>
                                         </div>
                                     )
                                 }
