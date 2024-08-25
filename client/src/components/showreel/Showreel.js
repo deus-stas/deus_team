3187d5e8fd777684e0e5aaec761fc6058bffeadc
Selezneva Anna <sovvysha@yandex.ru>
2024-08-25 21:17:53 +0300

    update

--- client/src/components/showreel/Showreel.js
+++ client/src/components/showreel/Showreel.js
@@ -52,7 +52,7 @@ const Showreel = (props) => {
                     <div ref={showReelRef}  className="showreel__s" onClick={handlePlay} >
                         {
                             data.video && data.video !== 'undefined' && data.video !== 'null' &&
-                            <video ref={videoRef} poster={showPng} loop playsInline preload={'auto'}>
+                            <video ref={videoRef}  poster={showPng} loop playsInline preload={'auto'}>
                                 <source src={data.video ? `${apiUrl}/uploads/${data.video.filename}` : null}
                                         type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;"/>
                             </video>
