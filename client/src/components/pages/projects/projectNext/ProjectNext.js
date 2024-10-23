71378675503760bc429c0f5c6b3a898b5bb6fc95
Arthur-DevionFox <blackberserkfox@mail.ru>
2024-10-23 18:25:31 +0300

    Убрана анимация скролла до верхней части сайта

--- client/src/components/pages/projects/projectNext/ProjectNext.js
+++ client/src/components/pages/projects/projectNext/ProjectNext.js
@@ -46,6 +46,13 @@ const ProjectNext = ({ props, detail }) => {
         videoRefs.current.push(ref);
     };
 
+    const returnToTheTop = (e) => {
+        const elements = document.querySelectorAll('.project-next__item');
+        elements.forEach((element) => {
+            window.scroll(0, 0)
+        })
+    }
+
     return (
         <section className="project-next">
             <h1 className="heading-primary">Ещё проекты</h1>
@@ -55,7 +62,7 @@ const ProjectNext = ({ props, detail }) => {
                     const isVideo = project.imageMob && project.imageMob?.filename.endsWith('.mp4') || project.image && project.image.filename.endsWith('.mp4');
                     return (
                         <DelayedLink key={index} to={`/projects/${project.nameInEng}`} datahash="toUp"
-                                     onClick={(e) => gotoAnchor(e)}>
+                                     onClick={returnToTheTop}>
                             <div className="project-next__item">
                                 {isVideo ?
                                     <span className="projects__item">
