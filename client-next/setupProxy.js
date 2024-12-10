d50ab4b914a8dbc05c8ce469030a7d1a9d88645c
stanislavmuhin <stnslvmukhin@gmail.com>
2024-12-10 19:23:03 +0300

    init

---
+++ client-next/setupProxy.js
@@ -0,0 +1,22 @@
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_LOCALHOST_URI : "http://v3.deus.team",
+            changeOrigin: true,
+            secure: false,
+            timeout:10000
+        })
+    );
+    app.use(
+        '/uploads',
+        createProxyMiddleware({
+            target: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_LOCALHOST_URI : "http://v3.deus.team",
+            changeOrigin: true,
+            secure: false,
+            timeout:10000
+        })
+    );
+};
