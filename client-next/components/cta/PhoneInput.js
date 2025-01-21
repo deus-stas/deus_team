55780a7edb864369a2026b2ddf234c361546d790
a9500484054 <a9500484054@yandex.ru>
2025-01-21 23:23:54 +0300

    next_0.1.20

--- client-next/components/cta/PhoneInput.js
+++ client-next/components/cta/PhoneInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const PhoneInput = () => {
+const PhoneInput = ({ setFieldValue }) => {
   const [phone, setPhone] = useState('+7 (___) ___-__-__');
 
   const handleInput = (e) => {
@@ -26,6 +26,9 @@ const PhoneInput = () => {
     const formattedValue = formatPhone(value);
     setPhone(formattedValue);
 
+    // Передаем значение в Formik
+    setFieldValue('phone', formattedValue);
+
     // Восстанавливаем позицию курсора
     window.requestAnimationFrame(() => {
       input.selectionStart = cursorPosition;
@@ -48,7 +51,9 @@ const PhoneInput = () => {
 
       const value = phone.replace(/\D/g, '').slice(1); // Оставляем только редактируемые цифры
       const newValue = value.slice(0, -1); // Удаляем последнюю цифру
-      setPhone(formatPhone(newValue));
+      const formattedValue = formatPhone(newValue);
+      setPhone(formattedValue);
+      setFieldValue('phone', formattedValue); // Обновляем значение в Formik
     }
 
     const allowedKeys = ['Delete', 'ArrowLeft', 'ArrowRight', 'Tab'];
