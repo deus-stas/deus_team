b310eb62c56a9ba2022640bbb0fcb7871ca35011
Serdar Hojayev <serdarhoja@gmail.com>
2025-03-24 15:57:43 +0100

    build error fixing

--- client-next/app/projects/page.js
+++ client-next/app/projects/page.js
@@ -3,10 +3,7 @@ import ClientWrapper from './ClientWrapper';
 async function getProjects() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROTOCOL}://${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/api/projects/`, { 
-      cache: 'no-store',
-      headers: {
-        'Cache-Control': 'no-cache'
-      }
+      next: { revalidate: 60 },
     });
     if (!res.ok) throw new Error('Failed to fetch projects');
     return res.json();
@@ -19,10 +16,7 @@ async function getProjects() {
 async function getThemes() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROTOCOL}://${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/api/themes/`, { 
-      cache: 'no-store',
-      headers: {
-        'Cache-Control': 'no-cache'
-      }
+      next: { revalidate: 60 }
     });
     if (!res.ok) throw new Error('Failed to fetch themes');
     const data = await res.json();
@@ -40,10 +34,7 @@ async function getThemes() {
 async function getTypes() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROTOCOL}://${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/api/types/`, { 
-      cache: 'no-store',
-      headers: {
-        'Cache-Control': 'no-cache'
-      }
+      next: { revalidate: 60 }
     });
     if (!res.ok) throw new Error('Failed to fetch types');
     const data = await res.json();
