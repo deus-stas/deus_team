aaf986d8641e7494f22c0ea7571332c1fb785b38
Serdar Hojayev <serdarhoja@gmail.com>
2025-04-02 00:11:50 +0200

    updated sitemap

--- client-next/next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-// client-next/next-sitemap.config.js
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://deus.team',
-    generateRobotsTxt: true, // optional
-    sitemapSize: 7000,
-};
-  
