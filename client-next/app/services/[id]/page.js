0ab56ba9f6d52131b96a5b17f53c5264914a8365
stanislavmuhin <stnslvmukhin@gmail.com>
2025-10-03 17:21:32 +0300

    og image

--- client-next/app/services/[id]/page.js
+++ client-next/app/services/[id]/page.js
@@ -80,7 +80,7 @@ export async function generateMetadata({ params }) {
           title: serviceData?.seoTitle || "Cервис",
           description: serviceData?.seoDescription || "Cервис",
           type: 'website',
-          images:[baseUrl+'/'+serviceData.serviceBanner.path],
+          images:[baseUrl+'/'+serviceData.serviceBanner?.path || baseUrl+'/deus.png'],
           url:baseUrl+"/services/"+serviceData.path
       },
       twitter: {},
